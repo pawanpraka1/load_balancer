c280529f74717709a6c044ca57c5374fbd2c2a8a
pawan <pawanprakash101@gmail.com>
2014-10-03 23:24:38 +0530

    added more info in stats

--- server/stats_handler.c
+++ server/stats_handler.c
@@ -17,15 +17,19 @@ int stats_read_req(server_info_t *server, int efd)
 int ad_stats(server_info_t *server_head, char *buf, u32bits cur_len)
 {
 	int len;
+	u32bits count = 1;
 	server_info_t *sinfo = server_head;
 	while (sinfo) {
 		len = snprintf(&buf[cur_len], BUF_LEN - cur_len,
-				"(%u)->(%u)\nread event = %u\nwrite events = %u\nbuf_len = %u buf_read = %u\n\n",
-				sinfo->id, sinfo->session->server ? sinfo->session->server->id : -1, 
+				"%u. (%u)->(%u)\nread event = %u\nwrite events = %u\nbuf_len = %u buf_read = %u\n\n",
+				count, sinfo->id, sinfo->session->server ? sinfo->session->server->id : -1, 
 				sinfo->read_events, sinfo->write_events, sinfo->session->buf_len, sinfo->session->buf_read);
 		cur_len += len;
 		sinfo = sinfo->next;
+		count++;
 	}
+	len = snprintf(&buf[cur_len], BUF_LEN - cur_len, "TOTAL COUNT = %u\n\n", count - 1);
+	cur_len += len;
 	return cur_len;
 }
 int stats_write_res(server_info_t *server)
@@ -35,13 +39,15 @@ int stats_write_res(server_info_t *server)
 	if (server->write_events == 1) {
 		if (!strncmp(server->session->buf, STATS, server->session->buf_len)) {
 			len = snprintf(server->session->buf, BUF_LEN,
-					"\nLB STATS :-\nclient connection = %u\npending connection = %u\n"
-					"read events = %u\nwrite events = %u\n\nBACKEND STATS :-\n",
-					lb_server->cur_conn, lb_server->cur_pending_conn, 
-					lb_server->read_events, lb_server->write_events);
+					"\nLB STATS :-\ncur client connection = %u\npending connection = %u\n"
+					"tot_conn = %u\n\nBACKEND STATS :-\n",
+					lb_server->cur_conn, lb_server->cur_pending_conn, lb_server->read_events);
 			server->session->buf_len = len;
 			bserver_info_t *bserv = bserver_head;
 			while (bserv) {
+				server->session->buf_len += snprintf(&server->session->buf[server->session->buf_len], 
+				BUF_LEN - server->session->buf_len, "\nBACKEND SERVER (%s:%hu) tot_conn %hd\n\n", 
+				bserv->ip_str, bserv->port, bserv->cur_conn);
 				server->session->buf_len += ad_stats(bserv->bserver, server->session->buf, server->session->buf_len);
 				bserv= bserv->bnext;
 			}
